f5040f572ab5a832bbed19d262100585bd37c592
Pratikchitruk <99415272+Pratikchitruk@users.noreply.github.com>
2022-03-23 22:50:10 +0530

    Add files via upload

---
+++ PasswordSpecialCharacter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REflections
+{
+   public class PasswordSpecialCharacter
+    {
+        string password;
+        string passwordis;
+
+
+        public PasswordSpecialCharacter(string e, string i)
+        {
+
+            password = e;
+            passwordis = i;
+
+        }
+        [NewAttribute("Accesosr", "give Password")]
+        public string getPassword()
+        {
+            return password;
+        }
+        [NewAttribute("Accesosr", "Pratik@123")]
+        public string getPasswordId()
+        {
+            return passwordis;
+        }
+    }
+}
