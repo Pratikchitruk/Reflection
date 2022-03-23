f4061c71916f9feab2644786799621915291344c
Pratikchitruk <99415272+Pratikchitruk@users.noreply.github.com>
2022-03-23 22:49:00 +0530

    Add files via upload

---
+++ PasswordNumaricNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REflections
+{
+    public class PasswordNumaricNumber
+    {
+        string password;
+        string passwordis;
+
+
+        public PasswordNumaricNumber(string e, string i)
+        {
+
+            password = e;
+            passwordis = i;
+
+        }
+        [NewAttribute("Accesosr", "give Password")]
+        public string getPassword()
+        {
+            return password;
+        }
+        [NewAttribute("Accesosr", "Pratik@123")]
+        public string getPasswordId()
+        {
+            return passwordis;
+        }
+    }
+}
