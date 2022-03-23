e060a7e37e5f63be29918d2607810f8a706855ca
PratikChitruk <pratik.chitruk@gmail.com>
2022-03-23 22:41:25 +0530

    Program.cs push for reflection

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace REflections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to reflections");
+
+            NewAttribute.AttributeDisplay(typeof(FirsttName));
+            Console.WriteLine("\n");
+            NewAttribute.AttributeDisplay(typeof(LastName));
+            Console.WriteLine("\n");
+            NewAttribute.AttributeDisplay(typeof(PhoneNo));
+            Console.WriteLine("\n");
+            NewAttribute.AttributeDisplay(typeof(Email));
+            Console.WriteLine("\n");
+            NewAttribute.AttributeDisplay(typeof(Password));
+            Console.WriteLine("\n");
+            NewAttribute.AttributeDisplay(typeof(PasswordUpperCase));
+            Console.WriteLine("\n");
+            NewAttribute.AttributeDisplay(typeof(PasswordNumaricNumber));
+            Console.WriteLine("\n");
+            NewAttribute.AttributeDisplay(typeof(PasswordSpecialCharacter));
+            Console.WriteLine("\n");
+        }
+    }
+}
